40a37e03b3cec2c22868ab55e52b87e10cb37cab
Jing <nwsuaf_lj@163.com>
2026-01-23 16:51:16 +0800

    Update 02_syri.sh

--- 07. Analysis of Y-linked variations/01. Identification of MSY large inversions/02_syri.sh
+++ 07. Analysis of Y-linked variations/01. Identification of MSY large inversions/02_syri.sh
@@ -17,6 +17,6 @@ nucmer --maxmatch -c 500 -b 500 -l 1000 -t 8 ${ref_name}.fa ${qry_name}.ragtag.f
 delta-filter -m -i 95 -l 1000 ${qry_name}.ragtag.delta > ${qry_name}.ragtag.i95_l1000.delta
 show-coords -THrd ${qry_name}.ragtag.i95_l1000.delta > ${qry_name}.ragtag.i95_l1000.coords
 #rm ${qry_name}.delta
-/share/home/zhanglab/user/nielei/miniconda3/envs/syri/bin/syri -c ${qry_name}.ragtag.i95_l1000.coords -d ${qry_name}.ragtag.i95_l1000.delta -r ${ref_name}.fa -q ${qry_name}.ragtag.fa --prefix ${qry_name}.ragtag
+syri -c ${qry_name}.ragtag.i95_l1000.coords -d ${qry_name}.ragtag.i95_l1000.delta -r ${ref_name}.fa -q ${qry_name}.ragtag.fa --prefix ${qry_name}.ragtag
 plotsr --sr ${qry_name}.ragtagsyri.out --genomes ${qry_name}.genomes.txt -H 3 -W 5 -o ${qry_name}.ragtagsyri.pdf
 
