1c9a500e8405a7165a6f23075f6a66efb036d537
Qingyang Ni <nqy@zju.edu.cn>
2026-08-14 13:53:02 +0800

    Update run_validation_examples.sh

--- 04. Evolutionary analysis of Yq12 DYZ repeats/03. Classification of DYZ repeats into different evolutionary age groups/run_validation_examples.sh
+++ 04. Evolutionary analysis of Yq12 DYZ repeats/03. Classification of DYZ repeats into different evolutionary age groups/run_validation_examples.sh
@@ -6,14 +6,14 @@ set -euo pipefail
 # redundant sequences were dereplicated. Separate FASTA files represent the
 # independently selected sequence batches used for validation.
 
-python validate_age_groups_by_kmer_pcoa.py --fasta KOR08.DYZ1.uniq.fa --annotations DYZ1.anno.tsv --outdir KOR08_DYZ1_L --k_values 15,21,25,31 --max_clusters 30
-python validate_age_groups_by_kmer_pcoa.py --fasta C129-CYI01.DYZ1.uniq.fa --annotations DYZ1.anno.tsv --outdir C129-CYI01_DYZ1_L --k_values 15,21,25,31 --max_clusters 30
-python validate_age_groups_by_kmer_pcoa.py --fasta C106-CZA06.DYZ1.uniq.fa --annotations DYZ1.anno.tsv --outdir C106-CZA06_DYZ1_L --k_values 15,21,25,31 --max_clusters 30
-python validate_age_groups_by_kmer_pcoa.py --fasta C068-CHA-NE08.DYZ1.uniq.fa --annotations DYZ1.anno.tsv --outdir C068-CHA-NE08_DYZ1_L --k_values 15,21,25,31 --max_clusters 30
-python validate_age_groups_by_kmer_pcoa.py --fasta C069-CHA-NE09.DYZ1.uniq.fa --annotations DYZ1.anno.tsv --outdir C069-CHA-NE09_DYZ1_L --k_values 15,21,25,31 --max_clusters 30
+python validate_age_groups_by_kmer_pcoa.py --fasta KOR08.DYZ1.uniq.fa --annotations DYZ1.anno.tsv --outdir KOR08_DYZ1 --k_values 21,25,31 --max_clusters 30
+python validate_age_groups_by_kmer_pcoa.py --fasta C129-CYI01.DYZ1.uniq.fa --annotations DYZ1.anno.tsv --outdir C129-CYI01_DYZ1 --k_values 21,25,31 --max_clusters 30
+python validate_age_groups_by_kmer_pcoa.py --fasta C106-CZA06.DYZ1.uniq.fa --annotations DYZ1.anno.tsv --outdir C106-CZA06_DYZ1 --k_values 21,25,31 --max_clusters 30
+python validate_age_groups_by_kmer_pcoa.py --fasta C068-CHA-NE08.DYZ1.uniq.fa --annotations DYZ1.anno.tsv --outdir C068-CHA-NE08_DYZ1 --k_values 21,25,31 --max_clusters 30
+python validate_age_groups_by_kmer_pcoa.py --fasta C069-CHA-NE09.DYZ1.uniq.fa --annotations DYZ1.anno.tsv --outdir C069-CHA-NE09_DYZ1 --k_values 21,25,31 --max_clusters 30
 
-python validate_age_groups_by_kmer_pcoa.py --fasta C076-CHA-NE16.Alu_SAT.uniq.fa --annotations DYZ2.anno.tsv --outdir C076-CHA-NE16_DYZ2 --k_values 15,21,25,31 --max_clusters 30
-python validate_age_groups_by_kmer_pcoa.py --fasta C110-CUG02.Alu_SAT.uniq.fa --annotations DYZ2.anno.tsv --outdir C110-CUG02_DYZ2 --k_values 15,21,25,31 --max_clusters 30
-python validate_age_groups_by_kmer_pcoa.py --fasta C051-CHA-N11.Alu_SAT.uniq.fa --annotations DYZ2.anno.tsv --outdir C051-CHA-N11_DYZ2 --k_values 15,21,25,31 --max_clusters 30
-python validate_age_groups_by_kmer_pcoa.py --fasta C137-CMH04.Alu_SAT.uniq.fa --annotations DYZ2.anno.tsv --outdir C137-CMH04_DYZ2 --k_values 15,21,25,31 --max_clusters 30
-python validate_age_groups_by_kmer_pcoa.py --fasta C042-CHA-N02.Alu_SAT.uniq.fa --annotations DYZ2.anno.tsv --outdir C042-CHA-N02_DYZ2 --k_values 15,21,25,31 --max_clusters 30
+python validate_age_groups_by_kmer_pcoa.py --fasta C076-CHA-NE16.Alu_SAT.uniq.fa --annotations DYZ2.anno.tsv --outdir C076-CHA-NE16_DYZ2 --k_values 21,25,31 --max_clusters 30
+python validate_age_groups_by_kmer_pcoa.py --fasta C110-CUG02.Alu_SAT.uniq.fa --annotations DYZ2.anno.tsv --outdir C110-CUG02_DYZ2 --k_values 21,25,31 --max_clusters 30
+python validate_age_groups_by_kmer_pcoa.py --fasta C051-CHA-N11.Alu_SAT.uniq.fa --annotations DYZ2.anno.tsv --outdir C051-CHA-N11_DYZ2 --k_values 21,25,31 --max_clusters 30
+python validate_age_groups_by_kmer_pcoa.py --fasta C137-CMH04.Alu_SAT.uniq.fa --annotations DYZ2.anno.tsv --outdir C137-CMH04_DYZ2 --k_values 21,25,31 --max_clusters 30
+python validate_age_groups_by_kmer_pcoa.py --fasta C042-CHA-N02.Alu_SAT.uniq.fa --annotations DYZ2.anno.tsv --outdir C042-CHA-N02_DYZ2 --k_values 21,25,31 --max_clusters 30
